d8f2227b1a1af37380fcb92bb17001aec54b53a7
Akshay1-ARJ <92629214+Akshay1-ARJ@users.noreply.github.com>
2022-11-27 23:41:56 +0530

    AJ
    AJ

---
+++ movie2.java
@@ -0,0 +1,25 @@
+package qwerty;
+
+public class movie2 {
+
+	
+		public static void main(String[] args) {
+			
+			 String str = "kannada";
+			  int cnt = 0;
+			  char[] inp = str.toCharArray();
+			  System.out.println("Duplicate Characters are:");
+			  for (int i = 0; i < str.length(); i++) {
+			   for (int j = i + 1; j < str.length(); j++) {
+			    if (inp[i] == inp[j]) {
+			     System.out.println(inp[j]);
+			     cnt++;
+			     break;
+			    }
+			   }
+			  }
+			 }
+
+	
+
+}
